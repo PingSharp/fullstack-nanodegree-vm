5e37abe36c09a31db0d428d0d7c4d3c498ce48da
PingSharp <dotping@online.de>
2019-02-25 15:01:59 +0100

    feat: add google login page with google oauth2 Api

--- vagrant/restaurantwithflask/project.py
+++ vagrant/restaurantwithflask/project.py
@@ -7,8 +7,20 @@ from flask import session as login_session
 import random,string
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,scoped_session
- 
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
+import httplib2
+import json
+from flask import make_response
+import requests
 from database_setup import Restaurant, Base, MenuItem
+
+CLIENT_ID = json.loads(
+    open('secret.json','r').read()
+)['web']['client_id']
+APPLICATION_NAME = "Restaurant Menu Application"
  
 engine = create_engine('sqlite:///restaurantmenu.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -81,6 +93,78 @@ def showLogin():
                     for x in xrange(32))
     login_session['state']=state
     return render_template('login.html',STATE=state)
+
+@app.route('/gconnect',methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'),401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    code = request.data
+
+    try:
+        oauth_flow = flow_from_clientsecrets('secret.json',scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'),401
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'%access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url,'GET')[1])
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')),500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dump("Token's user ID doesnt match given user ID"),401
+        )
+        response.header['Content-Type'] = 'application/json'
+        return response
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json"
+    params = {'access_token': credentials.access_token}
+    answer = requests.get(userinfo_url,params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
 @app.route('/')
 @app.route('/restaurants/')
 def restaurants():
