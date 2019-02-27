c2e7e2f337c933be970f04bf67cf011dd9b9461b
PingSharp <dotping@online.de>
2019-02-27 16:17:21 +0100

    feat: add new table

--- vagrant/restaurantwithflask/database_setup.py
+++ vagrant/restaurantwithflask/database_setup.py
@@ -12,7 +12,9 @@ class Restaurant(Base):
     id = Column(
         Integer,primary_key = True
     )
+    user_id = Column(Integer,ForeignKey('user.id'))
     menuitem = relationship("MenuItem",back_populates='restaurant',cascade="all, delete, delete-orphan")
+    user = relationship('User')
     @property
     def serialize(self):
         return {
@@ -21,7 +23,13 @@ class Restaurant(Base):
         }
     def __repr__(self):
         return "<Restaurant(name='%s')>"%self.name
-    
+
+class User(Base):
+    __tablename__ = 'user'
+    name = Column(String(80),nullable = False)
+    id = Column(Integer,primary_key = True)
+    email = Column(String(250),nullable = False)
+    picture = Column(String(250),nullable = True)
 
 class MenuItem(Base):
     __tablename__ = 'menuitem'
@@ -33,7 +41,9 @@ class MenuItem(Base):
     Restaurant_id = Column(
         Integer,ForeignKey('restaurant.id')
     )
+    user_id = Column(Integer,ForeignKey('user.id'))
     restaurant = relationship("Restaurant",back_populates="menuitem")
+    user = relationship('User')
 
     def __repr__(self):
         return "<MenuItem(name='%s',course='%s',description='%s',price='%s')>"%(self.name,self.course,self.description,self.price)
@@ -47,7 +57,7 @@ class MenuItem(Base):
             'course' : self.course,
         }
 engine = create_engine(
-    'sqlite:///restaurantmenu.db'
+    'sqlite:///restaurantmenuwithusers.db'
 )
 
 Base.metadata.create_all(engine)
